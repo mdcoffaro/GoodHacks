ebb9dfea04547431d4805570bc8b32586f5c9d35
Michael Coffaro <michaelcoffaro@Michaels-MacBook-Pro-5.local>
2015-06-07 03:26:58 -0700

    added framework for registration, needs css/db

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+/*GET register page. */
+router.get('/register', function(req, res, next) {
+	res.render('register.html');
+});
+
 module.exports = router;
