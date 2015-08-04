b438805bfde874ef1fdc9a3f3bb99d9a6b4dfb69
Michael Coffaro <michaelcoffaro@yahoo.com>
2015-08-04 14:37:01 -0700

    figuring out http 500 error

--- public/js/studentProfile.js
+++ public/js/studentProfile.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-	Parse.initialize("yWusaetgryRjKz5aTG81tzEd4F40e7BSrse5Tpao", "jtBjEknzObCeEh0hYUVmiwTiITQXpKjo4QNigzzJ");
+/*$(document).ready(function () {
+	//Parse.initialize("yWusaetgryRjKz5aTG81tzEd4F40e7BSrse5Tpao", "jtBjEknzObCeEh0hYUVmiwTiITQXpKjo4QNigzzJ");
 
-	var currentUser = Parse.User.current();
+	//var currentUser = Parse.User.current();
 
-	//if(currentUser.id === )
-
-}
+	//console.log("Hello world");
+	//console.log(currentUser);
+});*/
