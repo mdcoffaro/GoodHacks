e0b983211fe6d0aee04959a66d6a92a1697e96b6
Michael Coffaro <michaelcoffaro@yahoo.com>
2015-08-05 19:51:17 -0700

    worked on student auth

--- public/js/studentProfile.js
+++ public/js/studentProfile.js
@@ -1,8 +1,22 @@
-/*$(document).ready(function () {
-	//Parse.initialize("yWusaetgryRjKz5aTG81tzEd4F40e7BSrse5Tpao", "jtBjEknzObCeEh0hYUVmiwTiITQXpKjo4QNigzzJ");
+$(document).ready(function () {
+	Parse.initialize("yWusaetgryRjKz5aTG81tzEd4F40e7BSrse5Tpao", "jtBjEknzObCeEh0hYUVmiwTiITQXpKjo4QNigzzJ");
 
-	//var currentUser = Parse.User.current();
+	var currentUser = Parse.User.current();
+	var loggedInUser = local_data;
 
-	//console.log("Hello world");
-	//console.log(currentUser);
-});*/
+	//check to make sure that the user accessing this profile is the correct logged in user, and not a rando
+
+	console.log(currentUser);
+
+	if(currentUser){
+		if(currentUser.attributes.email === loggedInUser[0].email){
+			console.log("We good");
+			//continue page load
+		}else{
+			//prevent page load, redirect to home page
+			window.location = "http://localhost:3000";
+		}
+	}else{
+		window.location = "http://localhost:3000";
+	}	
+});
